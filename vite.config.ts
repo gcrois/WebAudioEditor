f1ad4c453f17194b557b1d7c18d7437d968e5c40
Gregory Croisdale <gregtc@umich.edu>
2024-08-08 14:47:36 -0400

    fixed baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/DeckFlow/",
+	base: "/WebAudioEditor/",
 	plugins: [
 		wasm(),
 		topLevelAwait(),
